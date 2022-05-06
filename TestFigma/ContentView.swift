a364f5e13ef045e0e18fea567d879e45127de46f
Mahdi.Hanifeh <hanifehmahdi3@gmail.com>
2022-05-06 12:33:44 +0430

    Initial Commit

---
+++ TestFigma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestFigma
+//
+//  Created by MahdiHanifeh on 2/16/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestFigma/TestFigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFigmaApp.swift
+//  TestFigma
+//
+//  Created by MahdiHanifeh on 2/16/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct TestFigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
